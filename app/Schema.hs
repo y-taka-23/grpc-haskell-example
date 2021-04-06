0a4189a7c7c0a51d04ae951c82b363ce65ace113
Yuto Takahashi <ytaka23dev@gmail.com>
2021-04-07 05:17:24 +0900

    Add the GetFeature RPC

--- app/Schema.hs
+++ app/Schema.hs
@@ -1,10 +1,40 @@
 {-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE DeriveAnyClass  #-}
+{-# LANGUAGE DeriveGeneric   #-}
 {-# LANGUAGE PolyKinds       #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies    #-}
 
 module Schema where
 
-import Mu.Quasi.GRpc
+import Data.Aeson   ( FromJSON )
+import Data.Text    as T
+import GHC.Generics ( Generic )
+import GHC.Int      ( Int32 )
+
+import Mu.Quasi.GRpc ( grpc )
+import Mu.Schema     ( FromSchema, ToSchema )
 
 grpc "TheSchema" id "route_guide.proto"
+
+data Point = Point {
+      latitude  :: Int32
+    , longitude :: Int32
+    } deriving (
+      Eq, Show, Generic
+    , ToSchema   TheSchema "Point"
+    , FromSchema TheSchema "Point"
+    )
+
+instance FromJSON Point
+
+data Feature = Feature {
+      name     :: T.Text
+    , location :: Maybe Point
+    } deriving (
+      Eq, Show, Generic
+    , ToSchema   TheSchema "Feature"
+    , FromSchema TheSchema "Feature"
+    )
+
+instance FromJSON Feature
--- app/Server.hs
+++ app/Server.hs
@@ -1,16 +1,75 @@
+{-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
 {-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
 
 module Main where
 
-import Mu.GRpc.Server
-import Mu.Server
+import           Control.Exception.Safe
+    ( MonadThrow
+    , SomeException
+    , catch
+    , displayException
+    , throwString
+    )
+import           Control.Monad          ( when )
+import           Control.Monad.IO.Class ( MonadIO, liftIO )
+import           Control.Monad.Reader   ( MonadReader, ask, runReaderT )
+import           Data.Aeson             ( eitherDecodeStrict' )
+import qualified Data.ByteString        as B
+import           Mu.GRpc.Server         ( msgProtoBuf, runGRpcAppTrans )
+import           Mu.Server
+    ( MonadServer
+    , SingleServerT
+    , method
+    , singleService
+    )
+import           Prelude                hiding ( readFile )
+import           System.Directory       ( doesFileExist )
+import           System.Exit            ( die )
 
-import Schema
+import Schema as S
 
 main :: IO ()
-main = runGRpcApp msgProtoBuf 8080 server
+main = do
+    savedFeatures <- loadFeatures "route_guide_db.json" `catch` fatal
+    runGRpcAppTrans msgProtoBuf 10000 (flip runReaderT savedFeatures) server
+        where
+            fatal = (\(e :: SomeException) -> die $ displayException e)
 
-server :: MonadServer m => SingleServerT i RouteGuide m _
-server = singleService ()
+server
+    :: (MonadServer m, MonadReader [S.Feature] m)
+    => SingleServerT i S.RouteGuide m _
+server = singleService (method @"GetFeature" getFeature)
+
+getFeature
+    :: (MonadServer m, MonadReader [S.Feature] m)
+    => S.Point -> m S.Feature
+getFeature p = do
+    fs <- ask
+    case filter ((== Just p) . location) fs of
+        []  -> pure $ Feature "" (Just p)
+        f:_ -> pure f
+
+loadFeatures
+    :: (MonadThrow m, MonadIO m)
+    => FilePath -> m [S.Feature]
+loadFeatures fp = readFile fp >>= decodeFeatures
+
+readFile
+    :: (MonadThrow m, MonadIO m)
+    => FilePath -> m B.ByteString
+readFile fp = do
+    ok <- liftIO $ doesFileExist fp
+    when (not ok) $ throwString $ "failed to load default features: " ++ fp
+    liftIO $ B.readFile fp
+
+decodeFeatures
+    :: (MonadThrow m)
+    => B.ByteString -> m [S.Feature]
+decodeFeatures bs = case eitherDecodeStrict' bs of
+    Left err -> throwString $ "failed to load default features: " ++ err
+    Right fs -> pure fs
