0af60f3f23977582594d17a21fab7cd389de9545
Yuto Takahashi <ytaka23dev@gmail.com>
2021-04-10 20:17:54 +0900

    Add the RecordRoute RPC

--- app/Schema.hs
+++ app/Schema.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE DataKinds       #-}
 {-# LANGUAGE DeriveAnyClass  #-}
 {-# LANGUAGE DeriveGeneric   #-}
+{-# LANGUAGE DerivingVia     #-}
 {-# LANGUAGE PolyKinds       #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies    #-}
+{-# LANGUAGE TypeOperators   #-}
 
 module Schema where
 
@@ -13,7 +15,12 @@ import GHC.Generics ( Generic )
 import GHC.Int      ( Int32 )
 
 import Mu.Quasi.GRpc ( grpc )
-import Mu.Schema     ( FromSchema, ToSchema )
+import Mu.Schema
+    ( CustomFieldMapping(CustomFieldMapping)
+    , FromSchema
+    , Mapping((:->))
+    , ToSchema
+    )
 
 grpc "TheSchema" id "route_guide.proto"
 
@@ -47,3 +54,23 @@ data Feature = Feature {
     )
 
 instance FromJSON Feature
+
+data RouteSummary = RouteSummary {
+      pointCount   :: Int32
+    , featureCount :: Int32
+    , distance     :: Int32
+    , elapsedTime  :: Int32
+    } deriving (
+      Eq, Show, Generic
+    ) deriving (
+      ToSchema   TheSchema "RouteSummary"
+    , FromSchema TheSchema "RouteSummary"
+    ) via (
+      CustomFieldMapping "RouteSummary" RouteSummaryFieldMapping RouteSummary
+    )
+
+type RouteSummaryFieldMapping = '[
+      "pointCount" ':-> "point_count"
+    , "featureCount" ':-> "feature_count"
+    , "elapsedTime" ':-> "elapsed_time"
+    ]
--- app/Server.hs
+++ app/Server.hs
@@ -15,25 +15,33 @@ import           Control.Exception.Safe
     , displayException
     , throwString
     )
-import           Control.Monad          ( when )
-import           Control.Monad.IO.Class ( MonadIO, liftIO )
-import           Control.Monad.Reader   ( MonadReader, ask, runReaderT )
-import           Data.Aeson             ( eitherDecodeStrict' )
-import qualified Data.ByteString        as B
-import           Data.Conduit           ( ConduitT, Void, runConduit, (.|) )
-import qualified Data.Conduit.List      as CL
-import           Mu.GRpc.Server         ( msgProtoBuf, runGRpcAppTrans )
+import           Control.Monad            ( when )
+import           Control.Monad.IO.Class   ( MonadIO, liftIO )
+import           Control.Monad.Reader     ( MonadReader, ask, runReaderT )
+import           Data.Aeson               ( eitherDecodeStrict' )
+import qualified Data.ByteString          as B
+import           Data.Conduit             ( ConduitT, Void, runConduit, (.|) )
+import qualified Data.Conduit.Combinators as CC
+import qualified Data.Conduit.List        as CL
+import           Data.List                ( find )
+import           Data.Maybe               ( isJust )
+import           Data.Time
+    ( diffUTCTime
+    , getCurrentTime
+    , nominalDiffTimeToSeconds
+    )
+import           Mu.GRpc.Server           ( msgProtoBuf, runGRpcAppTrans )
 import           Mu.Server
     ( MonadServer
     , SingleServerT
     , method
     , singleService
     )
-import           Prelude                hiding ( readFile )
-import           System.Directory       ( doesFileExist )
-import           System.Exit            ( die )
+import           Prelude                  hiding ( readFile )
+import           System.Directory         ( doesFileExist )
+import           System.Exit              ( die )
 
-import Schema as S
+import qualified Schema as S
 
 main :: IO ()
 main = do
@@ -48,6 +56,7 @@ server
 server = singleService (
       method @"GetFeature" getFeature
     , method @"ListFeatures" listFeatures
+    , method @"RecordRoute" recordRoute
     )
 
 getFeature
@@ -55,20 +64,35 @@ getFeature
     => S.Point -> m S.Feature
 getFeature p = do
     fs <- ask
-    case filter ((== Just p) . location) fs of
-        []  -> pure $ Feature "" (Just p)
+    case filter ((== Just p) . S.location) fs of
+        []  -> pure $ S.Feature "" (Just p)
         f:_ -> pure f
 
 listFeatures
     :: (MonadServer m, MonadReader [S.Feature] m)
-    => S.Rectangle -> ConduitT Feature Void m () -> m ()
+    => S.Rectangle -> ConduitT S.Feature Void m () -> m ()
 listFeatures r sink = do
     fs <- ask
     runConduit $ CL.sourceList fs .| CL.filter inRectangle .| sink
         where
-            inRectangle f = case location f of
+            inRectangle f = case S.location f of
                 Nothing -> False
                 Just p  -> inRange r p
+
+recordRoute
+    :: (MonadServer m, MonadReader [S.Feature] m, MonadIO m)
+    => ConduitT () S.Point m () -> m S.RouteSummary
+recordRoute src = do
+    t0 <- liftIO getCurrentTime
+    fs <- ask
+    let start = S.Point 0 0
+        countFc p = if inFeatures fs p then 1 else 0
+        record (pc, fc, d, lastP) p =
+            (pc + 1, fc + countFc p, d + calcDistance lastP p, p)
+    (pc, fc, d, _) <- runConduit $ src .| CC.foldl record (0, 0, 0, start)
+    t1 <- liftIO getCurrentTime
+    let et = round $ nominalDiffTimeToSeconds $ diffUTCTime t1 t0
+    pure $ S.RouteSummary pc fc d et
 
 loadFeatures
     :: (MonadThrow m, MonadIO m)
@@ -91,13 +115,30 @@ decodeFeatures bs = case eitherDecodeStrict' bs of
     Right fs -> pure fs
 
 inRange :: S.Rectangle -> S.Point -> Bool
-inRange r p = case (lo r, hi r) of
+inRange r p = case (S.lo r, S.hi r) of
     (Just l, Just h) ->
-        left <= longitude p && longitude p <= right &&
-        bottom <= latitude p  && latitude p <= top
+        left <= S.longitude p && S.longitude p <= right &&
+        bottom <= S.latitude p  && S.latitude p <= top
             where
-                left = longitude l `min` longitude h
-                right = longitude l `max` longitude h
-                bottom = latitude l `min` latitude h
-                top = latitude l `max` latitude h
+                left = S.longitude l `min` S.longitude h
+                right = S.longitude l `max` S.longitude h
+                bottom = S.latitude l `min` S.latitude h
+                top = S.latitude l `max` S.latitude h
     (_, _) -> False
+
+inFeatures :: [S.Feature] -> S.Point -> Bool
+inFeatures fs p = isJust $ find ((==) (Just p) . S.location) fs
+
+calcDistance :: (Integral i) => S.Point -> S.Point -> i
+calcDistance lastP p = round $ radius * central
+    where
+        coordToRadian d = (fromIntegral d * pi) / (180 * 10000000) :: Double
+        lat1 = coordToRadian $ S.latitude lastP
+        lat2 = coordToRadian $ S.latitude p
+        lng1 = coordToRadian $ S.longitude lastP
+        lng2 = coordToRadian $ S.longitude p
+        (dlat, dlng) = (lat2 - lat1, lng2 - lng1)
+        central = 2 * (asin $ sqrt $
+            sin (dlat/2) * sin (dlat/2) +
+            cos lat1 * cos lat2 * sin (dlng/2) * sin (dlng/2))
+        radius = 6371000
